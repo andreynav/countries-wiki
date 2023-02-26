d83d2ab63ee9836dbb2121fb6eefd1985fa64373
andreinalivaika <nav.testsw@gmail.com>
2023-02-26 14:34:15 +0300

    3 add structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+import { ReactComponent as LogoSVG } from '../../assets/logo.svg'
+
+export const App = () => {
+  return (
+    <AppWrapper>
+      <header className="App-header">
+        <LogoSVG />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </AppWrapper>
+  )
+}
+
+const AppWrapper = styled.div`
+  display: grid;
+  height: 100vh;
+  width: auto;
+  background-color: var(--bg-color);
+  color: white;
+  text-align: center;
+
+  & .App-link {
+    color: #61dafb;
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1rem minmax(20rem, 1fr) 1rem;
+  }
+
+  @media (max-width: 480px) {
+    grid-template-rows: 3rem auto 1fr;
+    grid-template-columns: 1rem minmax(10rem, 1fr) 1rem;
+  }
+`
