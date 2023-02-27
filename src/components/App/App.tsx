2f2994ca868dbb90ac59acbbfd51260918c740c9
andreinalivaika <nav.testsw@gmail.com>
2023-02-27 21:07:36 +0300

    4 add icon libs and header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,31 +1,33 @@
 import styled from 'styled-components'
 
-import { ReactComponent as LogoSVG } from '../../assets/logo.svg'
+// import { ReactComponent as LogoSVG } from '../../assets/logo.svg'
+import { Header } from '../Header/Header'
 
 export const App = () => {
   return (
     <AppWrapper>
-      <header className="App-header">
-        <LogoSVG />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/*<main className="App-header">*/}
+      {/*  <LogoSVG />*/}
+      {/*  <p>*/}
+      {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
+      {/*</main>*/}
     </AppWrapper>
   )
 }
 
 const AppWrapper = styled.div`
   display: grid;
-  height: 100vh;
+  //height: 100vh;
   width: auto;
   background-color: var(--bg-color);
   color: white;
