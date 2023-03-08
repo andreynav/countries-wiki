634fb5b8f5dc98f9bab351900bca7fcdd18d371f
andreinalivaika <nav.testsw@gmail.com>
2023-03-08 15:49:27 +0300

    15 add search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent } from 'react'
-import { IoSearch } from 'react-icons/io5'
+import { IoCloseCircleOutline, IoSearch } from 'react-icons/io5'
 import styled from 'styled-components'
 
 export const Search = ({ search, setSearch }: any) => {
@@ -7,32 +7,30 @@ export const Search = ({ search, setSearch }: any) => {
 
   return (
     <InputContainer>
-      <IoSearch />
+      <IoSearch size={20} />
       <Input value={search} onChange={onInputChange} />
+      <IoCloseCircleOutline size={20} onClick={() => setSearch('')} />
     </InputContainer>
   )
 }
 
 const InputContainer = styled.label`
   display: grid;
-  grid-template-columns: 16px auto;
+  grid-template-columns: 20px auto 20px;
   align-items: center;
-  //min-width: 100%;
   background-color: var(--colors-ui-base);
   border-radius: var(--radii);
   box-shadow: var(--shadow);
   margin-bottom: 0;
-  padding: 1rem 2rem;
+  padding: 1rem;
   cursor: pointer;
 
   @media (max-width: 767px) {
-    //grid-template-columns: auto;
     margin-bottom: 2rem;
   }
 
   @media (max-width: 480px) {
     margin-bottom: 2rem;
-    //width: auto;
   }
 `
 
