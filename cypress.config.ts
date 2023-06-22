31a80262e2ab3d061a2fb81d52f2eb310901dc9e
andreinalivaika <nav.testsw@gmail.com>
2023-06-22 10:11:34 +0300

    43 add cypress

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    defaultCommandTimeout: 5000,
+    baseUrl: 'http://localhost:3002/',
+    chromeWebSecurity: false,
+    viewportWidth: 1000,
+    viewportHeight: 800
+  }
+})
