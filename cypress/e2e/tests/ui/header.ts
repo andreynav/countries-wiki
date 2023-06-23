9d146be23a7fc5b909eff0521006b12b38551205
andreinalivaika <nav.testsw@gmail.com>
2023-06-23 08:32:26 +0300

    48 turn off onboarding by default

--- cypress/e2e/tests/ui/header.ts
+++ cypress/e2e/tests/ui/header.ts
@@ -1,4 +1,4 @@
-import { common, header, onboarding } from '../../pages'
+import { common, header } from '../../pages'
 
 describe('Header suite', () => {
   beforeEach(() => {
@@ -9,24 +9,20 @@ describe('Header suite', () => {
     cy.get(header.titleLocator).should('have.text', header.titleText)
   })
 
-  it('Default onboarding toggle should be on', () => {
-    onboarding.closeTooltip()
-    cy.get(header.onboardingIconLocator).should('have.descendants', 'circle')
+  it('Default onboarding toggle should be off', () => {
+    cy.get(header.onboardingIconLocator).should('not.have.descendants', 'circle')
   })
 
   it('Default theme toggle should be light', () => {
-    onboarding.closeTooltip()
     cy.get(header.themeTextLocator).should('have.text', 'light theme')
   })
 
   it('Switch onboarding toggle and check icon has been changed', () => {
-    onboarding.closeTooltip()
     cy.get(header.onboardingTextLocator).click()
-    cy.get(header.onboardingIconLocator).should('not.have.descendants', 'circle')
+    cy.get(header.onboardingIconLocator).should('have.descendants', 'circle')
   })
 
   it('Switch theme toggle and check background has been changed', () => {
-    onboarding.closeTooltip()
     cy.get(header.themeTextLocator).click()
     cy.get(header.headerLocator).should(
       'have.css',
